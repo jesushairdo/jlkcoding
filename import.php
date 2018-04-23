ea94c377deb965e1482be806a4fff20f35c901f3
Jamie Kerwick <jamie@jamiekerwick.co.uk>
2018-04-23 12:50:24 +0100

    using cleanupfunction

--- import.php
+++ import.php
@@ -7,31 +7,21 @@ use MicrosoftAzure\Storage\Table\Models\Entity;
 use MicrosoftAzure\Storage\Table\Models\EdmType;
 
 //Set connection String
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=jlkcoding;AccountKey=21rwyUroy2Es66UAMBeBu/9F1rvn0DGA6gsfo/1HgB9zG2XZWjDbsZVeDy5zDZJkIfZjDejdI1+5W+TfR1GHWw==";
+$connectionString = 'DefaultEndpointsProtocol=https;AccountName=jlkcoding;AccountKey=21rwyUroy2Es66UAMBeBu/9F1rvn0DGA6gsfo/1HgB9zG2XZWjDbsZVeDy5zDZJkIfZjDejdI1+5W+TfR1GHWw==';
 
 // Create Table REST proxy.
 $tableClient = TableRestProxy::createTableService($connectionString);
 
-//Characters not allowed in content of key fields
-// /
-// \
-// #
-// ?
-// U+0000 to U+001F
-//   including \t \n \r
-// U+007F to U+009F
-//$handle = fopen('data/GameGenieCodes-snes.csv','r');
+//Process file
 if (($handle = fopen('data/GameGenieCodes-snes.csv', 'r')) !== FALSE) {
     while (($import = fgetcsv($handle, 1000, ",")) !== FALSE) {
         $num = count($import);
-        echo "<p> $num fields in line $row: <br /></p>\n";
+        echo '<p> '. $num .' fields in line '.$row.': <br /></p>'."\n";
         $row++;
         for ($c=0; $c < $num; $c++) {
-            echo $import[$c] . "<br />\n";
+            echo cleanKeyValue($import[$c]) . '<br />'."\n";
         }
     }
     fclose($handle);
 }
-
-print_r($import);
 ?>
